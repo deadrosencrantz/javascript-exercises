a5c24379adf89e9f5f3a9586a9846590ee7429e5
deadrosencrantz <221909074+deadrosencrantz@users.noreply.github.com>
2025-08-27 15:43:11 +0100

    Complete 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr, ...removeThis) {
+    const newArray = [];
+    arr.forEach((item) => {
+        if (!removeThis.includes(item)){
+            newArray.push(item);
+        }
+    });
+    return  newArray;
+    }
 
 // Do not edit below this line
 module.exports = removeFromArray;
