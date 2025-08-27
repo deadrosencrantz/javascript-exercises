e09145586e9bf82926acaeeb4fde483ef30f76be
deadrosencrantz <221909074+deadrosencrantz@users.noreply.github.com>
2025-08-27 16:32:00 +0100

    Complete 11_tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInF) {
+  let tempInC = (tempInF -32) / 1.8;
+  let roundedTempInC = Math.round(tempInC * 10) / 10;
+  return roundedTempInC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInC) {
+  let tempInF = (tempInC * 1.8) + 32;
+  let roundedTempInF = Math.round(tempInF * 10) /10;
+  return roundedTempInF;
 };
 
 // Do not edit below this line
