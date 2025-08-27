646539effaf374915b1e202de15b9549c77518e2
deadrosencrantz <221909074+deadrosencrantz@users.noreply.github.com>
2025-08-27 15:27:19 +0100

    Complete 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,7 @@
-const reverseString = function() {
-
-};
+const reverseString = function(string) {
+    const newString = string.split("").reverse().join("");
+    return newString;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
